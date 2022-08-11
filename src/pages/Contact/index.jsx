3ea4c8a93f77562766258b1c1d6a142468698e45
Misael Paul <misaelpaulmamangun@gmail.com>
2022-08-11 14:41:11 +0800

    contact add space

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -2,7 +2,7 @@ import MainLayout from '../../layouts/Main'
 
 const Contact = () => {
   return (
-    <MainLayout className='mt-6 px-4'>
+    <MainLayout className='mt-6 px-4 pb-4'>
       <div className='container has-background-light p-4 box'>
         <div className='columns'>
           <div className='column'>
