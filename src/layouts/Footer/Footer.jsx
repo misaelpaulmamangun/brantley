5e4466012c7ff69665a55f5045fc0c3bbbeaf9f9
Misael Paul <misaelpaulmamangun@gmail.com>
2022-08-17 14:25:13 +0800

    update footer

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,41 +1,25 @@
 import css from './footer.module.css'
+import footer from './../../schemas/footer.json'
 
 const Footer = () => {
+  const { routes } = footer
   return (
     <>
       <footer className={`${css.footer} has-background-white`}>
         <div className='container'>
           <div className='columns'>
-            <div className='column'>
-              <p className='is-size-4'>About</p>
-              <ul>
-                <li>Reviews</li>
-                <li>How it works</li>
-              </ul>
-            </div>
-            <div className='column'>
-              <p className='is-size-4'>Company</p>
-              <ul>
-                <li>Pricing</li>
-                <li>Case studies</li>
-                <p>Products</p>
-              </ul>
-            </div>
-            <div className='column'>
-              <p className='is-size-4'>Support</p>
-              <ul>
-                <li>Documentation</li>
-                <li>Data securities</li>
-              </ul>
-            </div>
-            <div className='column'>
-              <p className='is-size-4'>Legal</p>
-              <ul>
-                <li>Terms and condition</li>
-                <li>Privacy policies</li>
-                <li>Cookie information</li>
-              </ul>
-            </div>
+            {routes.map((route, index) => (
+              <div className='column'>
+                <p className='is-size-4'>{route.title}</p>
+                <ul>
+                  {route.child.map((item, index) => (
+                    <li key={index}>
+                      <a href={item.link}>{item.title}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
           </div>
           <p>&copy; {new Date().getFullYear()} - All rights reserved.</p>
         </div>
